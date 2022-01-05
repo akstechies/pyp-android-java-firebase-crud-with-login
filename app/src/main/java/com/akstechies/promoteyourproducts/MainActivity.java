81c27ac04b97754050baef303aad7a78459b003d
akstechies <akstechies@gmail.com>
2022-01-05 23:49:24 +0530

    working on main activity product list

--- app/src/main/java/com/akstechies/promoteyourproducts/MainActivity.java
+++ app/src/main/java/com/akstechies/promoteyourproducts/MainActivity.java
@@ -1,24 +1,40 @@
 package com.akstechies.promoteyourproducts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button addProductBtn;
+    private RecyclerView productRV;
+    private ProgressBar loadingPB;
+    private FloatingActionButton addFAB;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ArrayList<ProductRVModel> productRVModelArrayList; 
+    private RelativeLayout mainRL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addProductBtn = findViewById(R.id.idBtnAddProduct);
+        addFAB = findViewById(R.id.idFABAddProduct);
 
-        addProductBtn.setOnClickListener(new View.OnClickListener() {
+        addFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, AddProductActivity.class);
